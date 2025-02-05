9ec1a73c5ead8717522c07285972961086a8ce0d
Carlos Rabelo <developer@carlosrabelo.com.br>
2025-02-05 07:12:00 -0400

    feat(z80): add string and number output routines

---
+++ src/z80/strings.asm
@@ -0,0 +1,8 @@
+; strings.asm - String constants for PBasic Z80
+; -----------------------------------------------------------------------
+
+STR_PROMPT:	db	"> ", 0
+STR_CRLF:	db	13, 10, 0
+MSG_BANNER:	db	"PBasic", 13, 10, 0
+MSG_OK:		db	"OK", 13, 10, 0
+MSG_ERROR:	db	"?SYNTAX ERROR", 13, 10, 0
